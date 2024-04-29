3ec682b7915c232358f0b207c054324ea0b3d568
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-04-29 16:10:09 +0300

    Add new global states: SkipNextCallOfDropAllEquipment and SkipNextCallOfDropDropAllNearPawn.

--- GlobalState.cs
+++ GlobalState.cs
@@ -10,5 +10,7 @@ namespace ReequipWeaponUponRecovery
     {
         public static Config Config { get; set; } = new Config();
 
+        public static bool SkipNextCallOfDropAllEquipment { get; set;}
+        public static bool SkipNextCallOfDropDropAllNearPawn { get; set;}
     }
 }
