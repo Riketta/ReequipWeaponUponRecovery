3d57ac3751c44af85dac2b98f364ae1cde4b2e30
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-04-29 16:21:33 +0300

    Add docs to GlobalState states.

--- GlobalState.cs
+++ GlobalState.cs
@@ -12,7 +12,27 @@ namespace ReequipWeaponUponRecovery
 
         /// <summary>
         /// Mark that next DropAllEquipment call can be prevented if necessary.
+        /// 
+        /// Valid call chains to prevent execution:
+        /// "Pawn.DropAndForbidEverything -> Pawn_EquipmentTracker.DropAllEquipment";
+        /// "Pawn_EquipmentTracker.Notify_PawnSpawned -> Pawn_EquipmentTracker.DropAllEquipment".
+        /// 
+        /// Not valid (should be executed):
+        /// "Pawn.Strip -> Pawn_EquipmentTracker.DropAllEquipment".
+        /// </summary>
         public static bool CanSkipNextCallOfDropAllEquipment { get; set;}
+
+        /// <summary>
+        /// Mark that next DropDropAllNearPawn call can be prevented if necessary.
+        /// 
+        /// Valid call chains to prevent execution:
+        /// "Pawn.DropAndForbidEverything -> Pawn_InventoryTracker.DropAllNearPawn";
+        /// 
+        /// Not valid (should be executed):
+        /// "Pawn.Strip -> Pawn_InventoryTracker.DropAllNearPawn";
+        /// "Pawn.Strip -> Pawn_InventoryTracker.DropAllNearPawn";
+        /// "CaravanEnterMapUtility.DropAllInventory -> Pawn_InventoryTracker.DropAllNearPawn".
+        /// </summary>
         public static bool CanSkipNextCallOfDropDropAllNearPawn { get; set;}
     }
 }
