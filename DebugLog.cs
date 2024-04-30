d5fd2c6d20b12f13a45d5b6c71ff3ef8fbfed22d
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-04-30 15:02:10 +0300

    Reimplement DebugLog that combines both Harmony and HugsLib logging.

---
+++ DebugLog.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReequipWeaponUponRecovery
+{
+    internal class DebugLog
+    {
+        public static void Log(string message)
+        {
+            string timestamp = DateTime.Now.ToString("HH:mm:ss.fffffff");
+            FileLog.Log($"[{timestamp}] {message}");
+            GlobalState.Logger?.Trace(message);
+        }
+    }
+}
